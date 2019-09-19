7dc909698778c20712453ea051e9db3ca9af6bfe
WonderMad <onishgo@gmail.com>
2019-09-19 22:18:13 +0300

    linter fixes and some refactoring

--- src/bin/brain-calc.js
+++ src/bin/brain-calc.js
@@ -6,44 +6,39 @@ console.log('Welcome to the Brain Games!');
 console.log('What is the result of the expression?\n');
 const name = askName();
 console.log(`Hello ${name}\n`);
-// функция для генерации случайного выражения и вывода на экран
+// функция для генерации случайного выражения и вывода на экран с рассчетом результата
 const expression = () => {
   const randomOperator = getRandomInt(1, 3);
   const num1 = getRandomInt(1, 50);
   const num2 = getRandomInt(1, 50);
-  const multiNum1 = getRandomInt(1, 20);
-  const multiNum2 = getRandomInt(1, 10);
+  const multiNum1 = getRandomInt(1, 10);
+  const multiNum2 = getRandomInt(1, 20);
   if (randomOperator === 1) {
-    console.log(`${num1} + ${num2}`);
+    console.log(`Question: ${num1} + ${num2}`);
     return `${num1 + num2}`;
   }
   if (randomOperator === 2) {
-    console.log(`${num1} - ${num2}`);
+    console.log(`Question: ${num1} - ${num2}`);
     return `${num1 - num2}`;
   }
-  if (randomOperator === 3) {
-    console.log(`${multiNum1} * ${multiNum2}`);
-    return `${multiNum1 * multiNum2}`;
-  }
+  console.log(`Question: ${multiNum1} * ${multiNum2}`);
+  return `${multiNum1 * multiNum2}`;
 };
 // Игра
 const calcTrick = () => {
-  let i = 0;
+  let i = 1;
   while (i <= 3) {
-    if (i === 3) {
-      return console.log(`Congratsulations! ${name}`);
-    }
     const result = expression();
     const userAnswer = answer();
     if (userAnswer === result) {
       i += 1;
       console.log('correct!\n');
-    }
-    if (userAnswer !== result) {
+    } else {
       i = 0;
       console.log(`${userAnswer} is wrong answer. Correct answer was ${result}\nLet's try again, ${name}`);
     }
   }
+  console.log(`Congratsulations! ${name}`);
 };
 
 calcTrick();
--- src/bin/brain-even.js
+++ src/bin/brain-even.js
@@ -3,38 +3,28 @@
 import { askName, answer, getRandomInt } from '../index';
 
 console.log('Welcome to the Brain Games!');
-console.log('Answer "yes" if the number is even, otherwise answer "no".');
+console.log('Answer "yes" if the number is even, otherwise answer "no".\n');
 const name = askName();
-console.log(`Hello ${name}`);
+console.log(`Hello ${name}\n`);
 const isEvenTrick = () => {
   let i = 0;
   while (i <= 3) {
-    if (i === 3) {
-      return console.log(`Congratsulations! ${name}`);
-    }
     const randomNum = getRandomInt(1, 100);
     console.log(`Question: ${randomNum}`);
     const userAnswer = answer();
-    if (randomNum % 2 === 0 && userAnswer === 'yes') {
+    if ((randomNum % 2 === 0 && userAnswer === 'yes') || (randomNum % 2 === 1 && userAnswer === 'no')) {
       i += 1;
-      console.log('correct');
+      console.log('correct!\n');
     }
-    if (randomNum % 2 === 0 && userAnswer === 'no') {
+    if ((randomNum % 2 === 0 && userAnswer === 'no') || (randomNum % 2 === 1 && userAnswer === 'yes')) {
       i = 0;
-      console.log(`'no' is wrong answer. Correct answer was 'yes'. Let's try again, ${name}`);
-    }
-    if (randomNum % 2 === 1 && userAnswer === 'yes') {
-      i = 0;
-      console.log(`'yes' is wrong answer. Correct answer was 'no'. Let's try again, ${name}`);
-    }
-    if (randomNum % 2 === 1 && userAnswer === 'no') {
-      i += 1;
-      console.log('correct');
+      console.log(`'no' is wrong answer. Correct answer was 'yes'.\nLet's try again, ${name}\n`);
     }
     if (userAnswer !== 'no' && userAnswer !== 'yes') {
       i = 0;
-      console.log(`'${userAnswer}' is wrong answer. Answer 'yes' or 'no'.`);
+      console.log(`'${userAnswer}' is wrong answer. Answer 'yes' or 'no'.\n`);
     }
   }
+  console.log(`Congratsulations! ${name}`);
 };
 isEvenTrick();
