8f0a2a477bac5f74c49b60a7597a4493f36c39fc
WonderMad <onishgo@gmail.com>
2019-09-27 18:24:20 +0300

    fix

--- src/bin/Games/brain-prime.js
+++ src/bin/Games/brain-prime.js
@@ -3,22 +3,21 @@
 import { getRandomInt } from '../../index';
 import game from '../game-engine';
 import helloUserName from '../brain-games';
-// Приветствие
+
 const userName = helloUserName();
 
 
 const isPrime = () => {
   const randomNum = getRandomInt(2, 101);
   let i = 2;
-  console.log(randomNum);
+  console.log(`question: ${randomNum}`);
   while (i <= randomNum) {
     if (randomNum % i !== 0) {
       i += 1;
-    } else if (randomNum % i === 0 && i !== randomNum) {
+    } else if (randomNum % i === 0 && i < randomNum) {
       return 'no';
-    }
+    } else return 'yes';
   }
-  return 'yes';
 };
 
 game(isPrime, userName);
