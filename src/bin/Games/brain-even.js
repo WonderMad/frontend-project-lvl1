6d1cc57dd5b60bc3ff86e9cc7cb33c1ab48da88c
WonderMad <onishgo@gmail.com>
2019-09-27 17:27:33 +0300

    fix

---
+++ src/bin/Games/brain-even.js
@@ -0,0 +1,33 @@
+#!/usr/bin/env node
+
+import { askName, answer, getRandomInt } from '../index';
+
+// Приветствие
+console.log('Welcome to the Brain Games!');
+console.log('Answer "yes" if the number is even, otherwise answer "no".\n');
+const name = askName();
+console.log(`Hello ${name}\n`);
+
+// Игра
+const isEvenGame = () => {
+  let i = 1;
+  while (i <= 3) {
+    const randomNum = getRandomInt(1, 100);
+    console.log(`Question: ${randomNum}`);
+    const userAnswer = answer();
+    if ((randomNum % 2 === 0 && userAnswer === 'yes') || (randomNum % 2 === 1 && userAnswer === 'no')) {
+      i += 1;
+      console.log('correct!\n');
+    }
+    if ((randomNum % 2 === 0 && userAnswer === 'no') || (randomNum % 2 === 1 && userAnswer === 'yes')) {
+      i = 1;
+      console.log(`'no' is wrong answer. Correct answer was 'yes'.\nLet's try again, ${name}\n`);
+    }
+    if (userAnswer !== 'no' && userAnswer !== 'yes') {
+      i = 1;
+      console.log(`'${userAnswer}' is wrong answer. Answer 'yes' or 'no'.\n`);
+    }
+  }
+  console.log(`Congratsulations! ${name}`);
+};
+isEvenGame();
