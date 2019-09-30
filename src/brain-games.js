a97021d39b39529d0d5175d8a9460d40e2d43219
WonderMad <onishgo@gmail.com>
2019-09-30 19:32:08 +0300

    ++

---
+++ src/brain-games.js
@@ -0,0 +1,12 @@
+#!/usr/bin/env node
+
+import { askName } from './index';
+
+const helloUserName = () => {
+  console.log('Welcome to the Brain Games!');
+  const name = askName();
+  console.log(`Hello ${name}`);
+  return name;
+};
+
+export default helloUserName;
