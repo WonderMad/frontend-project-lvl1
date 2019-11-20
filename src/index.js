de1e3f0bba7cc62b4efb25d696a35ecb71554808
WonderMad <onishgo@gmail.com>
2019-11-20 15:38:17 +0300

    game-engine merged to index

--- src/index.js
+++ src/index.js
@@ -1,3 +1,29 @@
-const getRandomInt = (min, max) => Math.floor(Math.random() * (max - min + 1)) + min;
+import * as readlineSync from 'readline-sync';
 
-export default getRandomInt;
+const answersCount = 3;
+
+const game = (gameData) => {
+  const gameIntro = gameData()[0];
+  console.log('Welcome to the Brain Games!');
+  console.log(gameIntro);
+  const userName = readlineSync.question('May I have your name? ');
+  console.log(`hello ${userName}`);
+  for (let i = 1; i <= answersCount; i += 1) {
+    const values = gameData();
+    const question = `Question: ${values[1]}`;
+    const result = values[2];
+    console.log(question);
+    const answer = readlineSync.question('Your answer: ');
+    if (result === answer) {
+      console.log('correct\n');
+    }
+    if (result !== answer) {
+      i = 0;
+      console.log(`${answer} is wrong answer.Correct answer was ${result}`);
+      console.log(`Let's try again, ${userName}`);
+    }
+  }
+  console.log(`Congratsulations! ${userName}`);
+};
+
+export default game;
