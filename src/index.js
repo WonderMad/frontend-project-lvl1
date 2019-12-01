edc38fe55a8c03b6dea6f669a235c84317e531bb
WonderMad <onishgo@gmail.com>
2019-12-01 14:08:32 +0300

    refact

--- src/index.js
+++ src/index.js
@@ -2,17 +2,14 @@ import * as readlineSync from 'readline-sync';
 
 const answersCount = 3;
 
-const game = (gameData) => {
-  const gameIntro = gameData()[0];
+const game = (gameIntro, gameData) => {
   console.log('Welcome to the Brain Games!');
   console.log(gameIntro);
   const userName = readlineSync.question('May I have your name? ');
   console.log(`hello ${userName}`);
   for (let i = 1; i <= answersCount; i += 1) {
-    const values = gameData();
-    const question = `Question: ${values[1]}`;
-    const result = values[2];
-    console.log(question);
+    const [question, result] = gameData();
+    console.log(`question ${question}`);
     const answer = readlineSync.question('Your answer: ');
     if (result === answer) {
       console.log('correct\n');
